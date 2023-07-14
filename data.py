f1af40e42d9cffe32baa3ddfcdcea316e0e7e2ee
lillian-mo <molillian05@gmail.com>
2023-07-13 22:06:06 -0400

    made function to filter for rebounds

--- data.py
+++ data.py
@@ -5,6 +5,17 @@ import py_ball
 def load_game(data: str) -> DataFrame:
     return pd.read_json(data, lines=True)
 
+def game_rebs(df: DataFrame, oord: str) -> DataFrame:
+    if oord == "o":
+        new_df = df.query(f"eventType == 'REB' and shotClock == 24.0")
+    else:
+        new_df = df.query(f"eventType == 'REB' and shotClock != 24.0")
+    new_df = new_df.filter(items=["eventType", "shotClock", "gameClock", "playerId", "period"])
+    return new_df
+
+
 game2 = load_game("0042100302_events.jsonl")
-game2 = game2.query("eventType == 'DRIBBLE'")
-print(game2)
+game2_oreb = game_rebs(game2, 'o')
+game2_dreb = game_rebs(game2, 'd')
+print(game2_oreb)
+print(game2_dreb)
