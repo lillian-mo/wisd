724623387c14275ffb186bf7cc16e2e89756e48c
lillian-mo <molillian05@gmail.com>
2023-07-14 22:51:24 -0400

    made events list

--- data.py
+++ data.py
@@ -5,18 +5,18 @@ import py_ball
 def load_game(data: str) -> DataFrame:
     return pd.read_json(data, lines=True)
 
-def game_rebs(df: DataFrame, oord: str) -> DataFrame:
+def game_rebs(df: DataFrame, oord: str, event_id: int) -> DataFrame:
     if oord == "o":
-        new_df = df.query(f"eventType == 'REB' and shotClock == 24.0")
+        new_df = df.query(f"eventType == '{evs[event_id]}' and shotClock == 24.0")
     else:
-        new_df = df.query(f"eventType == 'REB' and shotClock != 24.0")
+        new_df = df.query(f"eventType == '{evs[event_id]}' and shotClock != 24.0")
     new_df = new_df.filter(items=["eventType", "shotClock", "gameClock", "playerId", "period"])
     return new_df
 
-
 game2 = load_game("0042100302_events.jsonl")
-game2_oreb = game_rebs(game2, 'o')
-game2_dreb = game_rebs(game2, 'd')
+print(game2)
+evs = game2.eventType.unique().tolist()
+game2_oreb = game_rebs(game2, 'o', 4)
+game2_dreb = game_rebs(game2, 'd', 4)
 print(game2_oreb)
-print(game2_dreb)
 print(game2_dreb)
