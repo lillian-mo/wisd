28018aaa8351fa9a11aad1f2a4435680a09945b4
lillian-mo <molillian05@gmail.com>
2023-07-14 23:19:49 -0400

    abstracted game_rebs function into game_evs

--- data.py
+++ data.py
@@ -5,18 +5,18 @@ import py_ball
 def load_game(data: str) -> DataFrame:
     return pd.read_json(data, lines=True)
 
-def game_rebs(df: DataFrame, oord: str, event_id: int) -> DataFrame:
-    if oord == "o":
-        new_df = df.query(f"eventType == '{evs[event_id]}' and shotClock == 24.0")
-    else:
-        new_df = df.query(f"eventType == '{evs[event_id]}' and shotClock != 24.0")
+def game_evs(df: DataFrame, event_id: int) -> DataFrame:
+    new_df = df.query(f"eventType == '{evs[event_id]}'")
     new_df = new_df.filter(items=["eventType", "shotClock", "gameClock", "playerId", "period"])
     return new_df
 
 game2 = load_game("0042100302_events.jsonl")
 print(game2)
 evs = game2.eventType.unique().tolist()
-game2_oreb = game_rebs(game2, 'o', 4)
-game2_dreb = game_rebs(game2, 'd', 4)
+print(evs)
+game2_oreb = game_evs(game2, 4).query(f"shotClock != 24.0")
+game2_dreb = game_evs(game2, 4).query(f"shotClock == 24.0")
+game2_touch = game_evs(game2, 0)
 print(game2_oreb)
 print(game2_dreb)
+print(game2_touch)
