ea2cbff34af81ad2f31e097e42d19dbb7ee68b55
lillian-mo <molillian05@gmail.com>
2023-07-15 22:59:59 -0400

    added ball location during time of rebound

--- data.py
+++ data.py
@@ -7,16 +7,16 @@ def load_game(data: str) -> DataFrame:
 
 def game_evs(df: DataFrame, event_id: int) -> DataFrame:
     new_df = df.query(f"eventType == '{evs[event_id]}'")
-    new_df = new_df.filter(items=["eventType", "shotClock", "gameClock", "playerId", "period"])
     return new_df
 
-game2 = load_game("0042100302_events.jsonl")
+game2_events = load_game("0042100302_events.jsonl")
+game2_track = load_game("0042100302_tracking.jsonl").drop_duplicates(subset=['gameClock', 'period'])
+game2_merged = game2_events.merge(game2_track, how='inner', on=['gameClock', 'period'])
+game2 = game2_merged.filter(items=["eventType", "shotClock_x", "gameClock", "period", "ball"])
 print(game2)
 evs = game2.eventType.unique().tolist()
 print(evs)
-game2_oreb = game_evs(game2, 4).query(f"shotClock != 24.0")
-game2_dreb = game_evs(game2, 4).query(f"shotClock == 24.0")
-game2_touch = game_evs(game2, 0)
+game2_oreb = game_evs(game2, 4).query(f"shotClock_x != 24.0").drop('eventType', axis=1)
+game2_dreb = game_evs(game2, 4).query(f"shotClock_x == 24.0").drop('eventType', axis=1)
 print(game2_oreb)
 print(game2_dreb)
-print(game2_touch)
