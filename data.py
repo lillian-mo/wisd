bfc76357b1118301bf6e1363c6e7a351d650d5ae
lillian-mo <molillian05@gmail.com>
2023-07-24 22:45:51 -0400

    added corresponding rebound to danger chance

--- data.py
+++ data.py
@@ -2,7 +2,6 @@ import pandas as pd
 from pandas import DataFrame
 from py_ball import playbyplay
 from typing import Union
-import numpy as np
 
 HEADERS = {'Connection': 'keep-alive',
            'Host': 'stats.nba.com',
@@ -28,6 +27,12 @@ event_dict = {
     8: 'SUB',
     9: 'TMO',
     10: 'JB'
+}
+
+danger_type = {
+    0: 3,
+    1: 5,
+    2: 4
 }
 
 
@@ -76,30 +81,54 @@ def game_evs(df: DataFrame, events: Union[list, int], team: str='none', oord: st
     
 
     new_coords = pd.DataFrame(new_df.pop('ball').tolist(), index=new_df.index, columns = ['x','y','z'])
-    combined = pd.concat([new_df, new_coords.reindex(new_df.index)], axis=1).dropna()
+    combined = pd.concat([new_df, new_coords.reindex(new_df.index)], axis=1).dropna().drop(columns='z')
 
     return combined.drop(columns=['gameId'])
-    return new_df
 
-def in_ranges(time: float, period: int, ranges: list) -> bool:
+def in_ranges(time: float, period: int, game: int, team: str, ranges: list, kept: list=[]) -> Union[bool, list]:
     time_int = int(time * 100)
     for i in ranges:
-        if time_int >= i[0] and time_int <= i[1] and period == i[2]:
-            return True
+        if time_int >= i[0] and time_int <= i[1] and period == i[2] and game == i[3] and team == i[4]:
+            return [True, i[1]]
     
-    return False
+    return [False, False]
 
-def high_danger(df: DataFrame, rebs: DataFrame, shots: DataFrame) -> DataFrame:
+
+def high_danger(rebs: DataFrame, shots: DataFrame) -> DataFrame:
     reb_ranges = []
-    shot_copy = shots
     n = rebs.gameClock.values.tolist()
     p = rebs.period.values.tolist()
+    g = rebs.GAME_ID.values.tolist()
+    t = rebs.team.values.tolist()
     for i in range(len(n)- 1):
         num = n[i]
-        reb_ranges.append([int(num * 100) - 1000, int(num * 100), p[i]])
+        reb_ranges.append([int(num * 100) - 1000, int(num * 100), p[i], int(g[i]), t[i]])
     
-    shot_copy['inRange'] = shot_copy.apply(lambda x: in_ranges(x.gameClock, x.period, reb_ranges), axis=1)
-    shot_copy = shot_copy.query('inRange == True')
-    shot_copy = shot_copy.drop(columns=['inRange'])
+    shots['inRange'] = shots.apply(lambda x: in_ranges(x.gameClock, x.period, int(x.GAME_ID), x.team, reb_ranges), axis=1)
+    range_pop = pd.DataFrame(shots.pop('inRange').tolist(), index=shots.index, columns=['bool', 'time'])
+    shots_new = pd.concat([shots, range_pop.reindex(shots.index)], axis=1).\
+                drop(columns=['EVENTNUM', 'shotClock_x', 'eventType']).\
+                rename(columns={'EVENTMSGTYPE': 'shotType', 'x': 'shot_x', 'y': 'shot_y'})
+    
+    rebs['int_time'] = (rebs['gameClock'] * 100).astype(int)
 
-    return shot_copy
+    together = pd.merge(rebs, shots_new, left_on=['int_time', 'period', 'GAME_ID'], right_on=['time', 'period', 'GAME_ID'], how='left')
+    together['shotType'] = together['shotType'].fillna(0).astype(int)
+    together['danger'] = together['shotType'].map(danger_type)
+    together = together.drop(columns=['shotClock_x', 'EVENTNUM', 'EVENTMSGTYPE', 'int_time', 'team_y', 'shotType', 'time'])
+    return together
+
+
+
+# leaving this here so you know how to call the functions this is just my testing
+"""
+game_ids = ['0042100301', '0042100302', '0042100303', '0042100304', '0042100305', '0042100306', '0042100307',\
+            '0042100311', '0042100312', '0042100313', '0042100314', '0042100315', '0042100401', '0042100402',\
+            '0042100403', '0042100404', '0042100405', '0042100406']
+
+some_games = mult_games(game_ids[1:3])
+rebounds = game_evs(some_games, 4, 'Heat', 'd')
+shots = game_evs(some_games, [1, 2], 'Heat')
+danger_shots = high_danger(rebounds, shots)
+print(danger_shots)
+"""
